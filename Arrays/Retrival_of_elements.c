eb9df171c98da502fb7ac4c8f056109b9408f888
ShauryShobit <72372993+ShauryShobit@users.noreply.github.com>
2021-04-04 11:24:31 +0530

    Add files via upload

---
+++ Arrays/Retrival_of_elements.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[100];
+    int n;
+    printf("Enter number of elements you want to store in array: ");
+    scanf("%d", &n);
+    printf("\nEnter Elements: \n");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nYour stored array: \n");
+
+    for(int i = 0; i < n; i++){
+        printf("%d", arr[i]);
+        printf(" ");
+    }
+    return 0;
+}
